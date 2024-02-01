db3e1e96e12f1c57fcc00b1fb6776cb8003ae192
DIS-Wolv <thomas.lieumont@gmail.com>
2024-02-01 18:47:10 +0100

    modification des listes du garrage

--- template_functions/WOLV_Garage/DefineA3.hpp
+++ template_functions/WOLV_Garage/DefineA3.hpp
@@ -199,7 +199,7 @@ class RscProgressBar_WolvGUIgarage
 	style = ST_HORIZONTAL;
 	colorFrame[] = {0, 0, 0, 0.8};
 	colorBar[] = {1, 1, 1, 1};
-	texture = "ProgressBar_Bg.paa";
+	texture = "#(argb,8,8,3)color(1,1,1,1)";
 	font = "puristaMedium";
 	sizeEx = 0;
 	lineSpacing = 0;
@@ -213,190 +213,66 @@ class RscProgressBar_WolvGUIgarage
 };
 class RscListBox_WolvGUIgarage
 {
-	deletable = 0;
-	fade = 0;
-	access = 0;
-	type = 5;
-	rowHeight = 0;
-	colorText[] = 
-	{
-		1,
-		1,
-		1,
-		1
-	};
-	colorDisabled[] = 
-	{
-		1,
-		1,
-		1,
-		0.25
-	};
-	colorScrollbar[] = 
-	{
-		1,
-		0,
-		0,
-		0
-	};
-	colorSelect[] = 
-	{
-		0,
-		0,
-		0,
-		1
-	};
-	colorSelect2[] = 
-	{
-		0,
-		0,
-		0,
-		1
-	};
-	colorSelectBackground[] = 
-	{
-		0.95,
-		0.95,
-		0.95,
-		1
-	};
-	colorSelectBackground2[] = 
-	{
-		1,
-		1,
-		1,
-		0.5
-	};
-	colorBackground[] = 
-	{
-		0,
-		0,
-		0,
-		0.3
-	};
-	soundSelect[] = 
-	{
-		"\A3\ui_f\data\sound\RscListbox\soundSelect",
-		0.09,
-		1
-	};
-	autoScrollSpeed = -1;
-	autoScrollDelay = 5;
-	autoScrollRewind = 0;
-	arrowEmpty = "#(argb,8,8,3)color(1,1,1,1)";
-	arrowFull = "#(argb,8,8,3)color(1,1,1,1)";
-	colorPicture[] = 
-	{
-		1,
-		1,
-		1,
-		1
-	};
-	colorPictureSelected[] = 
-	{
-		1,
-		1,
-		1,
-		1
-	};
-	colorPictureDisabled[] = 
-	{
-		1,
-		1,
-		1,
-		0.25
-	};
-	colorPictureRight[] = 
-	{
-		1,
-		1,
-		1,
-		1
-	};
-	colorPictureRightSelected[] = 
-	{
-		1,
-		1,
-		1,
-		1
-	};
-	colorPictureRightDisabled[] = 
-	{
-		1,
-		1,
-		1,
-		0.25
-	};
-	colorTextRight[] = 
-	{
-		1,
-		1,
-		1,
-		1
-	};
-	colorSelectRight[] = 
-	{
-		0,
-		0,
-		0,
-		1
-	};
-	colorSelect2Right[] = 
-	{
-		0,
-		0,
-		0,
-		1
-	};
-	tooltipColorText[] = 
-	{
-		1,
-		1,
-		1,
-		1
-	};
-	tooltipColorBox[] = 
-	{
-		1,
-		1,
-		1,
-		1
-	};
-	tooltipColorShade[] = 
-	{
-		0,
-		0,
-		0,
-		0.65
-	};
-	class ListScrollBar
-	{
-		color[] = 
-		{
-			1,
-			1,
-			1,
-			1
-		};
-		autoScrollEnabled = 1;
-	};
-	x = 0;
-	y = 0;
-	w = 0.3;
-	h = 0.3;
-	style = 16;
-	font = "RobotoCondensed";
-	sizeEx = "(((((safezoneW / safezoneH) min 1.2) / 1.2) / 25) * 1)";
-	shadow = 0;
-	colorShadow[] = 
-	{
-		0,
-		0,
-		0,
-		0.5
-	};
-	period = 1.2;
-	maxHistoryDelay = 1;
+    deletable = 0;
+    fade = 0;
+    access = 0;
+    type = 5;
+    rowHeight = 0;
+    colorText[] = {1,1,1,1};
+    colorDisabled[] = {1,1,1,0.25};
+    colorScrollbar[] = {1,0,0,0};
+    colorSelect[] = {0,0,0,1};
+    colorSelect2[] = {0,0,0,1};
+    colorSelectBackground[] = {0.95,0.95,0.95,1};
+    colorSelectBackground2[] = {1,1,1,0.5};
+    colorBackground[] = {0,0,0,0.3};
+    soundSelect[] = {"\A3\ui_f\data\sound\RscListbox\soundSelect",0.09,1};
+    autoScrollSpeed = -1;
+    autoScrollDelay = 5;
+    autoScrollRewind = 0;
+    arrowEmpty = "#(argb,8,8,3)color(1,1,1,1)";
+    arrowFull = "#(argb,8,8,3)color(1,1,1,1)";
+    colorPicture[] = {1,1,1,1};
+    colorPictureSelected[] = {1,1,1,1};
+    colorPictureDisabled[] = {1,1,1,0.25};
+    colorPictureRight[] = {1,1,1,1};
+    colorPictureRightSelected[] = {1,1,1,1};
+    colorPictureRightDisabled[] = {1,1,1,0.25};
+    colorTextRight[] = {1,1,1,1};
+    colorSelectRight[] = {0,0,0,1};
+    colorSelect2Right[] = {0,0,0,1};
+    tooltipColorText[] = {1,1,1,1};
+    tooltipColorBox[] = {1,1,1,1};
+    tooltipColorShade[] = {0,0,0,0.65};
+    class ListScrollBar
+    {
+        color[] = {1,1,1,1};
+        autoScrollEnabled = 1;
+        colorActive[] = {1,1,1,1};
+        colorDisabled[] = {1,1,1,0.3};
+        thumb = "\A3\ui_f\data\gui\cfg\scrollbar\thumb_ca.paa";
+        arrowEmpty = "\A3\ui_f\data\gui\cfg\scrollbar\arrowEmpty_ca.paa";
+        arrowFull = "\A3\ui_f\data\gui\cfg\scrollbar\arrowFull_ca.paa";
+        border = "\A3\ui_f\data\gui\cfg\scrollbar\border_ca.paa";
+        shadow = 0;
+        scrollSpeed = 0.06;
+        width = 0;
+        height = 0;
+        autoScrollSpeed = -1;
+        autoScrollDelay = 5;
+        autoScrollRewind = 0;
+    };
+    x = 0;
+    y = 0;
+    w = 0.3;
+    h = 0.3;
+    style = 16;
+    font = "RobotoCondensed";
+    sizeEx = "(			(			(			((safezoneW / safezoneH) min 1.2) / 1.2) / 25) * 1)";
+    shadow = 0;
+    colorShadow[] = {0,0,0,0.5};
+    period = 1.2;
+    maxHistoryDelay = 1;
 };
 class RscButton_WolvGUIgarage
 {
